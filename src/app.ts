248bda18a64d562e98a4e364d0e202b6f33b554c
dvt <trinhvideo123@gmail.com>
2022-05-09 16:44:21 +0700

    add cors, compression and logging config

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,9 @@ import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import {Sequelize} from "sequelize-typescript";
 import {createServer, Server} from "http";
+import cors, {CorsOptions} from "cors";
+import compression from "compression";
+import morgan from "morgan";
 
 export default class App {
     private readonly app: express.Application;
@@ -11,6 +14,8 @@ export default class App {
     public readonly httpServer: Server;
     public readonly port: number;
     public readonly host: string;
+    private readonly corsOptions: CorsOptions;
+    private readonly logPattern: string;
 
     constructor() {
         this.app = express();
@@ -18,6 +23,11 @@ export default class App {
         this.httpServer = createServer(this.app);
         this.port = parseInt(process.env.PORT ?? "8888");
         this.host = process.env.HOST ?? "localhost";
+        this.corsOptions = {
+            origin: ["*"],
+            methods: ["GET", "POST", "PUT", "DELETE"]
+        };
+        this.logPattern = ":remote-addr [:date[clf]] :method :url :status - :response-time ms";
     }
 
     public start() {
@@ -37,6 +47,9 @@ export default class App {
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({extended: true}));
         this.app.use(cookieParser());
+        this.app.use(cors(this.corsOptions));
+        this.app.use(compression());
+        this.app.use(morgan(process.env.NODE_ENV === "production" ? this.logPattern : "dev"));
     }
 
     public getServer(): express.Application {
