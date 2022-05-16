530b99d194af5974e55b4c2dc9d3f9609ce27bba
trinhdvt <trinhvideo123@gmail.com>
2022-05-16 23:43:48 +0700

    add anonymous login feature

--- src/handler/GreetingHandler.ts
+++ src/handler/GreetingHandler.ts
@@ -1,15 +1,49 @@
 import {IOType, SocketType} from "../interfaces/SocketIO";
+import UserRepo from "../redis/models/User.redis";
+import StringUtils from "../utils/StringUtils";
 
 const registerGreetingHandler = (io: IOType, socket: SocketType) => {
-    console.log(`Client connected ${socket.id}`);
+    const sid = socket.id;
+    console.log(`Client connected ${sid}`);
 
     socket.on("message", (message: string) => {
         console.log(`Message received: ${message}`);
         io.emit("greeting", "We have new member");
     });
 
-    socket.on("disconnect", () => {
-        console.log(`Client disconnected ${socket.id}`);
+    socket.on("user:init", async callback => {
+        const userData = {
+            sid: sid,
+            name: StringUtils.randomName()
+        };
+
+        const userRepo = await UserRepo();
+        const user = await userRepo.createAndSave(userData);
+
+        callback({
+            ...userData,
+            id: user.entityId
+        });
+    });
+
+    socket.on("user:update", async arg => {
+        const userRepo = await UserRepo();
+        const {id, name} = arg;
+        const user = await userRepo.fetch(id);
+        if (user.entityId) {
+            user.name = name;
+            await userRepo.save(user);
+        }
+    });
+
+    socket.on("disconnect", async () => {
+        console.log(`Client disconnected ${sid}`);
+
+        const userRepo = await UserRepo();
+        const user = await userRepo.search().where("sid").eq(sid).returnFirst();
+        if (user) {
+            await userRepo.remove(user.entityId);
+        }
     });
 };
 
