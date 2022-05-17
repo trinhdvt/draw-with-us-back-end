ffcebd6055df7a859d351d0cc6aadf14173ac82d
dvt <trinhvideo123@gmail.com>
2022-05-17 15:28:11 +0700

    add logger using winston

--- src/middlewares/GlobalErrorHandler.ts
+++ src/middlewares/GlobalErrorHandler.ts
@@ -1,5 +1,6 @@
 import {ExpressErrorMiddlewareInterface, Middleware} from "routing-controllers";
 import {Service} from "typedi";
+import logger from "../utils/Logger";
 
 @Middleware({type: "after"})
 @Service()
@@ -7,12 +8,12 @@ export class GlobalErrorHandler implements ExpressErrorMiddlewareInterface {
     error(error: any, request: any, response: any, next: (err?: any) => any): void {
         if (error.hasOwnProperty("message") && !error.hasOwnProperty("httpCode")) {
             error = {
-                message: error.message
+                message: error.message,
+                httpCode: 500
             };
         }
 
-        console.error(`ERROR: ${JSON.stringify(error)}`);
-
+        logger.error(error);
         return response.status(error.httpCode ?? 500).json(error);
     }
 }
