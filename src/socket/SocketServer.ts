ddfde8b4a6773416b3ea01f9f97773517be40c83
dvt <trinhvideo123@gmail.com>
2022-06-18 11:16:24 +0700

    add socket.io/admin-ui

--- src/socket/SocketServer.ts
+++ src/socket/SocketServer.ts
@@ -1,7 +1,9 @@
 import * as http from "http";
+import {hostname} from "os";
 
 import {Server} from "socket.io";
 import {CorsOptions} from "cors";
+import {instrument} from "@socket.io/admin-ui";
 
 import {IOType} from "./SocketEvent";
 import {registerGameHandler, registerRoomHandler, registerUserHandler} from "./handler";
@@ -14,6 +16,16 @@ export default class SocketServer {
             SocketServer.io = new Server(httpServer, {
                 cors: cors
             });
+            instrument(SocketServer.io, {
+                auth: {
+                    type: "basic",
+                    username: "admin",
+                    password: "$2b$10$pkJpbPclw0/C2pOe533arOLx/pubsEh.xciCy8SwK0gvMFCV1DChG"
+                },
+                serverId: `${hostname()}#${process.pid}`,
+                namespaceName: "/admin"
+            });
+
             this.register();
         }
     }
